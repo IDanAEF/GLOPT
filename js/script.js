170e3df6fb4ce411b3a30eb18b0f3616be273928
Denis <efremov89deniss55@gmail.com>
2021-06-26 18:39:09 +0500

    it remains to make only footer

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.review__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+    infinite: true,
+    arrows: true
+  });
+});
